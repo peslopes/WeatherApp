3aa3bfbf40349e6d8f1f3c51753d9fc6cbec7223
Pedro <peslopes@inf.ufrgs.br>
2019-04-13 11:14:51 -0300

    created model structs

---
+++ WeatherApp/WeatherApp/Model/WeatherState.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherState.swift
+//  WeatherApp
+//
+//  Created by Pedro Enrique Sobrosa Lopes on 13/04/19.
+//  Copyright © 2019 WeatherApp. All rights reserved.
+//
+
+import Foundation
+
+enum WeatherState {
+    case snow
+    case sleet
+    case hail
+    case thunderstorm
+    case heavyRain
+    case lightRain
+    case showers
+    case heavyCloud
+    case lightCloud
+    case clear
+}
