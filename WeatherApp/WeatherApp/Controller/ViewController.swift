e0b35d5073c3bf566a16dc61b6b85ff32067b34b
Pedro <peslopes@inf.ufrgs.br>
2019-04-13 21:52:51 -0300

    Created storyboard skeleton

--- WeatherApp/WeatherApp/Controller/ViewController.swift
+++ WeatherApp/WeatherApp/Controller/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var weatherDescriptionLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
