309f51284d7971147dcb78730a5ddcb140b12ca7
Pedro <peslopes@inf.ufrgs.br>
2019-04-13 22:18:31 -0300

    Created some outlets from first screen

--- WeatherApp/WeatherApp/Controller/ViewController.swift
+++ WeatherApp/WeatherApp/Controller/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var weatherDescriptionLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
