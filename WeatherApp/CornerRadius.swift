b5138158fd4ab41fc49ad5e6371dbbb51a827528
Henrique Figueiredo Conte <henriqueconte6@gmail.com>
2019-04-15 17:30:44 -0300

    started linking day details

---
+++ WeatherApp/CornerRadius.swift
@@ -0,0 +1,23 @@
+//
+//  tryingCorner.swift
+//  WeatherApp
+//
+//  Created by Henrique Figueiredo Conte on 15/04/19.
+//  Copyright © 2019 WeatherApp. All rights reserved.
+//
+
+
+import UIKit
+
+@IBDesignable
+final class cornerRadius: UIView {
+    
+    @IBInspectable var cornerRadius: CGFloat = 10.0 {
+        didSet{
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = false
+            
+        }
+    }
+    
+}
---
+++ WeatherApp/WeatherApp/Controller/DayDetail.swift
@@ -0,0 +1,21 @@
+//
+//  DayDetail.swift
+//  WeatherApp
+//
+//  Created by Henrique Figueiredo Conte on 15/04/19.
+//  Copyright © 2019 WeatherApp. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DayDetail: UIViewController {
+
+    @IBOutlet weak var cityName: UILabel!
+    @IBOutlet weak var chanceOfRain: UILabel!
+    @IBOutlet weak var precipitation: UILabel!
+    @IBOutlet weak var humidity: UILabel!
+    @IBOutlet weak var visibility: UILabel!
+    @IBOutlet weak var uvIndex: UILabel!
+    @IBOutlet weak var pressure: UILabel!
+}
