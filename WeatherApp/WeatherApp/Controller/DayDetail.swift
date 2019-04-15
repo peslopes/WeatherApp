7d7a9704586a55b1a83e9846f54402216721b656
Henrique Figueiredo Conte <henriqueconte6@gmail.com>
2019-04-15 19:43:18 -0300

    included assets to details

--- WeatherApp/WeatherApp/Controller/DayDetail.swift
+++ WeatherApp/WeatherApp/Controller/DayDetail.swift
@@ -18,4 +18,38 @@ class DayDetail: UIViewController {
     @IBOutlet weak var visibility: UILabel!
     @IBOutlet weak var uvIndex: UILabel!
     @IBOutlet weak var pressure: UILabel!
+    @IBOutlet weak var colorBar: UIView!
+    
+    
+    
+    override func viewDidLoad() {
+        
+        setColorBar()
+    }
+    
+    
+    func setColorBar(){
+        
+        let redColor = UIColor(red: 1.0, green: 0.61, blue: 0.58, alpha: 0.6).cgColor
+        let blueColor = UIColor(red: 0.65 , green: 0.81, blue: 1.0, alpha: 0.6).cgColor
+        
+        var colorGradient = CAGradientLayer()
+        colorGradient.type = .radial
+        colorGradient.colors = [redColor, blueColor]
+        colorGradient.locations = [0.0, 2.0]
+        
+        colorGradient.frame = colorBar.frame
+        
+        print("Gradiente: ", colorGradient.frame)
+        print("Barra: ", colorBar.frame)
+        
+        
+        colorBar.layer.insertSublayer(colorGradient, at: 0)
+        
+        //colorBar = CAGradientLayer()
+        //colorBar.colors = [redColor, blueColor]
+    }
+    
 }
+
+
