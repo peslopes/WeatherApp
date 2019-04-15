bd0f707ff26698683e2ebfbf4892e4fd8180cbe0
Pedro <peslopes@inf.ufrgs.br>
2019-04-15 16:51:17 -0300

    Implemented parser to JSON from API

--- WeatherApp/WeatherApp/Controller/Parser.swift
+++ WeatherApp/WeatherApp/Controller/Parser.swift
@@ -9,6 +9,73 @@
 import Foundation
 
 class Parser {
+    func getDataFromJSON(json: String) -> [CityWeather] {
+        var cityWeatherInWeek: [CityWeather] = []
+        var dict: [String:String] = [:]
+        var itsAKey = true
+        var itsAValue = false
+        var key = ""
+        var value = ""
+        var started = false
+        var newDay = false
+        for char in json {
+            if started{
+                let putInKey = itsAKey && (char != ":" && char != "\\" && char != "{" && char != "\"" && char != ",")
+                let putInValue = itsAValue && (char != "," && char != "\\" && char != "}" && char != "\"")
+                if char == "]" {
+                    break
+                }
+                else if putInKey {
+                    key.append(char)
+                }
+                else if putInValue {
+                    value.append(char)
+                }
+                else if char == ":" {
+                    itsAKey = false
+                    itsAValue = true
+                    value = ""
+                }
+                else if char == "," {
+                    itsAValue = false
+                    if !newDay{
+                        dict[key] = value
+                    }
+                    else {
+                        newDay = false
+                    }
+                    itsAKey = true
+                    key = ""
+                }
+                else if char == "}" {
+                    dict[key] = value
+                    cityWeatherInWeek.append(CityWeather(id: Int(dict["id"] ?? ""), aplicableDate: dict["applicable_date"], weatherState: getWeatherStateFromAbbr(abbr: dict["weather_state_abbr"] ?? ""), windSpeedInMPH: Float(dict["wind_speed"] ?? ""), windDirection: Float(dict["wind_direction"] ?? ""), temp: Float(dict["the_temp"] ?? ""), maxTemp: Float(dict["max_temp"] ?? ""), minTemp: Float(dict["min_temp"] ?? ""), airPressureInMBAR: Float(dict["air_pressure"] ?? ""), humidity: Float(dict["humidity"] ?? ""), visibilityInMiles: Float(dict["visibility"] ?? ""), predictability: Int(dict["predictability"] ?? "")))
+                    itsAKey = true
+                    itsAValue = false
+                    dict = [:]
+                    key = ""
+                    newDay = true
+                }
+            }
+            else if char == "[" {
+                started = true
+            }
+        }
+        
+        
+        return cityWeatherInWeek
+    }
+    
+    private func getWeatherStateFromAbbr(abbr: String) -> WeatherState? {
+        var type: WeatherState?
+        WeatherState.allCases.forEach {
+            if abbr == $0.rawValue{
+                type = $0
+            }
+        }
+        return type
+    }
+    
     func getWOEID(string: String) -> String?{
         var key = ""
         var value = ""
@@ -44,4 +111,5 @@ class Parser {
         }
         return nil
     }
+    
 }
