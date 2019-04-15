05dd7c4c6ad6d0db1430ffdace41a3a87b368fcb
Pedro <peslopes@inf.ufrgs.br>
2019-04-15 12:04:18 -0300

    Created struct city localization

---
+++ WeatherApp/WeatherApp/Model/CityLocalization.swift
@@ -0,0 +1,16 @@
+//
+//  CityLocalization.swift
+//  WeatherApp
+//
+//  Created by Pedro Enrique Sobrosa Lopes on 15/04/19.
+//  Copyright © 2019 WeatherApp. All rights reserved.
+//
+
+import Foundation
+
+struct CityLocalization {
+    let title: String
+    let locationType: String
+    let woeid: Int
+    let lattLong: String
+}
