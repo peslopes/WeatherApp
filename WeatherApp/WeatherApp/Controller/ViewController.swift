1561ad35d4e75efaa65f3d1d592b8094d5e03294
Pedro <peslopes@inf.ufrgs.br>
2019-04-15 12:05:31 -0300

    Made first url request to get woeid

--- WeatherApp/WeatherApp/Controller/ViewController.swift
+++ WeatherApp/WeatherApp/Controller/ViewController.swift
@@ -12,12 +12,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var cityNameLabel: UILabel!
     @IBOutlet weak var weatherDescriptionLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
+    var cityWeather: CityWeather?
+    var woeid: String?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let session = URLSession.shared
+        let url = URL(string: "https://www.metaweather.com/api/location/search/?query=london")!
+        let task = session.dataTask(with: url, completionHandler: saveData(data:response:error:))
+//        let task = session.dataTask(with: url) { (data, _, _) -> Void in
+//            if let data = data {
+//                let string = String(data: data, encoding: String.Encoding.utf8)
+//                print(self.getWOEID(string: string!))
+//            }
+//        }
+        task.resume()
     }
-
+    
+    private func saveData(data: Data?, response: URLResponse?, error: Error?) {
+        if error == nil {
+            if let data = data {
+                let parser = Parser()
+                let string = String(data: data, encoding: String.Encoding.utf8) ?? ""
+                self.woeid = parser.getWOEID(string: string)
+                print(woeid ?? "")
+            }
+        }
+    }
 
 }
 
