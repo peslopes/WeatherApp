7a5cc5b38fdd2a6754552ad2268bb6d90e95c220
Henrique Figueiredo Conte <henriqueconte6@gmail.com>
2019-10-14 19:15:02 -0300

    started making backend from new screen

--- WeatherApp/WeatherApp/View/CityDetailsView.swift
+++ WeatherApp/WeatherApp/View/CityDetailsView.swift
@@ -6,9 +6,18 @@ import UIKit
 class CityDetailsView : UIViewController {
     
     @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var cityButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setInitialValues(name: "", buttonTitle: "")
+    }
+    
+    func setInitialValues(name: String, buttonTitle: String) {
+        cityNameLabel.text = name
+        cityNameLabel.font = UIFont(name: "Helvetica-Neue", size: 30)
+        cityButton.backgroundColor = .black
+        cityButton.setTitle(buttonTitle, for: .normal)
     }
     
 }
